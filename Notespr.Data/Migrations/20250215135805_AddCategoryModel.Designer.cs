f793867093932be093d6c915222858f9cb3a3932
Киравич Курова <jantsev.ruslan@yandex.ru>
2025-02-20 00:28:02 +0300

    LogicCategoryControoler-CategoryIndexView

--- Notespr.Data/Migrations/20250215135805_AddCategoryModel.Designer.cs
+++ Notespr.Data/Migrations/20250215135805_AddCategoryModel.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Notespr.Data.AppDbContext;
 
@@ -10,9 +11,11 @@ using Notespr.Data.AppDbContext;
 namespace Notespr.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250215135805_AddCategoryModel")]
+    partial class AddCategoryModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Notespr.Data/UnitOfWorks/IUnitOfWorks/IUnitOfWork.cs
+++ Notespr.Data/UnitOfWorks/IUnitOfWorks/IUnitOfWork.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using Notespr.Data.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Notespr.Data.Repository.IRepository
+namespace Notespr.Data.UnitOfWork.IUnitOfWorks
 {
     public interface IUnitOfWork
     {
-        ICategoryRepository Category{ get; }
+        ICategoryRepository Category { get; }
         //*todo сделать product логику для заметок
 
         void Save();
